894bfb60e74de1c518f6233788fc676fe52bae15
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-05-07 22:24:27 -0300

    register models to django admin site

--- backend/things/admin.py
+++ backend/things/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import ThingType, Thing
 
-# Register your models here.
+
+@admin.register(ThingType)
+class ThingTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'created_at',
+        'edited_at',
+    )
+
+    search_fields = (
+        'title',
+    )
+
+
+@admin.register(Thing)
+class ThingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'type',
+        'created_at',
+        'edited_at',
+    )
+
+    list_filters = (
+        'type',
+    )
+
+    autocomplete_fields = (
+        'type',
+    )
