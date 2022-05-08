2a51a202bd8b4562003f9d025e848df0d155fa26
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-05-07 22:24:09 -0300

    add Thing  and ThingType models

--- backend/things/models.py
+++ backend/things/models.py
@@ -1,3 +1,24 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class ThingType(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    title = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Thing(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    title = models.CharField(max_length=255)
+    type = models.ForeignKey(ThingType, on_delete=models.PROTECT)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
